209313ba12b597e7b1334feb5dfef8ac484573cf
Easteriv <1253070365@qq.com>
2020-11-11 22:52:38 +0800

    feat: init :tada:
    初始化快速开始框架

---
+++ src/main/java/com/cus/gf_work/GfWorkApplication.java
@@ -0,0 +1,16 @@
+package com.cus.gf_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaojiejun
+ */
+@SpringBootApplication
+public class GfWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GfWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cus/gf_work/GfWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cus.gf_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
